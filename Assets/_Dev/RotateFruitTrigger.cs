d129ece036ffff7edf68f75ec63ed10aaac8b7fd
Joshua <joshua.b.haack@gmail.com>
2025-09-21 16:49:48 +0200

    rotate fruits over mixer

---
+++ Assets/_Dev/RotateFruitTrigger.cs
@@ -0,0 +1,14 @@
+using DG.Tweening;
+using Fruits;
+using UnityEngine;
+
+public class RotateFruitTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.transform.TryGetComponent(out Fruit fruit))
+            return;
+        
+        fruit.transform.DORotate(Vector3.zero, 1);
+    }
+}
